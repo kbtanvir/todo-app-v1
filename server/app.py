aea5db5ac3442cf09ce6d7a6e9d0d7f84e5b801b
kbtanvir <tanvirkhaan004@gmail.com>
2024-07-01 01:43:53 +0600

    added api routes for crud operation

--- server/app.py
+++ server/app.py
@@ -1,11 +1,14 @@
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 
+# SQLAlchemy config
+# ---------------------
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,12 +22,18 @@ db = SQLAlchemy(model_class=Base)
 ma = Marshmallow(app)
 
 
+# MODEL config
+# ---------------------
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
 
+
+# * JSON serialization
+# ---------------------
 
 class TodoSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -34,10 +43,13 @@ class TodoSchema(ma.SQLAlchemyAutoSchema):
 todo_schema = TodoSchema()
 todos_schema = TodoSchema(many=True)
 
-db.init_app(app)
+db.init_app(app)  # implements sqlite db file
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db)  # implements migration files
 
+
+# API routes configuration
+# ---------------------
 
 @app.route("/")
 def helloWorld():
@@ -53,7 +65,54 @@ def get_todos():
     return jsonify(result)
 
 
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    title = request.json['title']
+    description = request.json['description']
+    completed = request.json['completed']
+
+    new_todo = Todo(title=title, description=description, complete=completed)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    return todo_schema.jsonify(new_todo)
+
+
+@app.route('/todos/<id>', methods=['GET'])
+def get_todo(id):
+    todo = Todo.query.get(id)
+    return todo_schema.jsonify(todo)
+
+
+@app.route('/todos/<id>', methods=['PUT'])
+def update_todo(id):
+    todo = Todo.query.get(id)
+
+    title = request.json['title']
+    description = request.json['description']
+    completed = request.json['completed']
+
+    todo.title = title
+    todo.description = description
+    todo.completed = completed
+
+    db.session.commit()
+
+    return todo_schema.jsonify(todo)
+
+
+@app.route('/todos/<id>', methods=['DELETE'])
+def delete_todo(id):
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+
+    return todo_schema.jsonify(todo)
+
+
 if __name__ == '__main__':
-    with app.app_context():
+
+    with app.app_context():  # must run flask app inside app context to ensure sql alchemy is working properly
         db.create_all()
         app.run(debug=True)
